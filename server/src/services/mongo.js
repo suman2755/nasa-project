07b90a7048dea354ba1d509db637660580117a18
Suman Kumar <suman.kumar@aventri.com>
2023-12-17 04:09:02 +0530

    Added continuous deployment

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL = "mongodb+srv://nasa-api:DtBEgt86PVdt5K69@cluster0.04op4vm.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
